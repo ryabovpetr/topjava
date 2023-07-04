378191e7da197c4fed9c00919be7ec39b1d89bdb
Ryabov Petr <ryabovpetr@gmail.com>
2023-07-04 14:26:26 +0400

    HW01 - add mealServlet and CRUD

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -5,6 +5,8 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class Meal {
+
+    private Integer id;
     private final LocalDateTime dateTime;
 
     private final String description;
@@ -15,6 +17,7 @@ public class Meal {
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
+        this.id = null;
     }
 
     public LocalDateTime getDateTime() {
@@ -35,5 +38,13 @@ public class Meal {
 
     public LocalTime getTime() {
         return dateTime.toLocalTime();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id){
+        this.id = id;
     }
 }
