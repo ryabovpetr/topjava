5ba9b13fc3971b186ef273cf8c2c9d5d72c1708b
Ryabov Petr <ryabovpetr@gmail.com>
2023-07-29 18:45:27 +0400

    meal hibernate entity, JpaMealRepository

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,19 +1,48 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import org.hibernate.validator.constraints.Range;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+@NamedQueries({
+        @NamedQuery(name = Meal.ALL_SORTED,
+                query = "SELECT m from Meal m WHERE m.user.id=:userId ORDER BY m.dateTime DESC"),
+        @NamedQuery(name = Meal.GET_BETWEEN_HALF_OPEN,
+                query = "SELECT m from Meal m WHERE m.user.id=:userId AND m.dateTime>=?1 AND m.dateTime<?2 " +
+                        "ORDER BY m.dateTime DESC"),
+        @NamedQuery(name = Meal.DELETE,
+                query = "DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:userId"),
+})
+@Entity
+@Table(name = "meal", uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "date_time"},
+        name = "meal_unique_user_datetime_idx")})
 public class Meal extends AbstractBaseEntity {
-    private LocalDateTime dateTime;
 
+    public static final String ALL_SORTED = "Meal.getAllSorted";
+    public static final String DELETE = "Meal.delete";
+    public static final String GET_BETWEEN_HALF_OPEN = "Meal.delete";
+
+    @Column(name = "date_time", nullable = false)
+    @NotNull
+    private LocalDateTime dateTime;
+    @Column(name = "description", nullable = false)
+    @NotBlank
+    @Size(min = 3, max = 128)
     private String description;
 
+    @Column(name = "calories", nullable = false)
+    @Range(min = 3, max = 5000)
     private int calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @NotNull
     private User user;
 
     public Meal() {
